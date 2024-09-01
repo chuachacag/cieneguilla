a5f96fb71eae3b1eba7bb71eab04cbae5c006304
chuachacag <christianjhonuncp01@gmail.com>
2024-09-01 12:16:45 -0500

    V2

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,3 +5,41 @@ document.querySelectorAll('.read-more').forEach(function(button) {
         this.textContent = textContent.classList.contains('show-more') ? 'Ver menos' : 'Ver más';
     });
 });
+
+document.querySelector('.menu-toggle').addEventListener('click', function() {
+    document.querySelector('.menu').classList.toggle('active');
+});
+/* START MENU DE CATEGORIAS */
+document.querySelectorAll('.menu li a').forEach(function(link) {
+    link.addEventListener('click', function(event) {
+        event.preventDefault();
+        
+        // Remover la clase 'active' de todos los enlaces
+        document.querySelectorAll('.menu li a').forEach(function(link) {
+            link.classList.remove('active');
+        });
+        
+        // Añadir la clase 'active' al enlace seleccionado
+        this.classList.add('active');
+        
+        // Ocultar todo el contenido con una transición
+        document.querySelectorAll('.category-content').forEach(function(content) {
+            content.classList.remove('active');
+        });
+        
+        // Mostrar el contenido seleccionado con una transición
+        const category = this.getAttribute('data-category');
+        document.getElementById(category).classList.add('active');
+        
+        // Si el menú está en modo móvil, cerrarlo después de seleccionar
+        if (window.innerWidth <= 768) {
+            document.querySelector('.menu').classList.remove('active');
+        }
+    });
+});
+
+// Activar el primer enlace y mostrar su contenido por defecto
+document.querySelector('.menu li a').classList.add('active');
+document.getElementById('categoria1').classList.add('active');
+
+/* END MENU DE CATEGORIAS */
